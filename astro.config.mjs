fe747555cbc277d7709488413de681657a4dd61a
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2023-08-16 09:30:34 -0400

    Change readme from template + edit base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nhs-t10.github.io',
-	base: '/website',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
