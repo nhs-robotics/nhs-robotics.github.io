9ff43c93d869b13888f726c08cd19aaad521f9c0
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2023-08-16 09:23:00 -0400

    Astro rewrite

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'T-10 Robotics';
+export const SITE_DESCRIPTION = 'T-10 (#4096) is a FIRST Tech Challenge (FTC) high school robotics team based at Needham High School in Needham, MA.';
