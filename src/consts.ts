7c1f7f426b181c20b517a23314fbfe82472b79de
Ancient Bison <ilansbernstein@gmail.com>
2024-11-14 21:14:02 -0500

    Update website description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "T-10 Robotics";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "FTC robotics team stationed at NHS";
